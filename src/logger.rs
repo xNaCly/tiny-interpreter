50408acdc66b36932189fc40ac0f72b54fc8dff1
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-03-08 16:17:44 +0100

    feat(logger): add logger::Logger::error function
    - this commit adds colors to logger output and removes the print prefix from logger::Logger::print

--- src/logger.rs
+++ src/logger.rs
@@ -1,4 +1,4 @@
-use std::{sync::Arc, time::SystemTime};
+use std::{process::exit, sync::Arc};
 pub const NONE: u8 = 0;
 pub const WARN: u8 = 1;
 pub const PRINT: u8 = 2;
@@ -7,15 +7,25 @@ pub const DEBUG: u8 = 3;
 #[derive(Debug)]
 pub struct Logger {
     pub loglevel: u8,
+    pub prefix: String,
 }
 
 impl Logger {
     fn format(&self, l_type: &str, l_txt: &str) -> String {
-        let now = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_millis();
-        format!("[{}] {}: {}", now, l_type, l_txt)
+        let color = match l_type {
+            "warn" => "\x1b[93m",
+            "debug" => "\x1b[96m",
+            "fatal error" => "\x1b[91m",
+            _ => "\x1b[0m",
+        };
+        if l_type.len() == 0 {
+            format!("{}: {}", self.prefix, l_txt)
+        } else {
+            format!(
+                "{}: {}{}{}: {}",
+                self.prefix, color, l_type, "\x1b[0m", l_txt
+            )
+        }
     }
 
     pub fn debug(&self, txt: &str) {
@@ -26,7 +36,7 @@ impl Logger {
 
     pub fn print(&self, txt: &str) {
         if self.loglevel >= PRINT {
-            println!("{}", self.format("print", txt));
+            println!("{}", self.format("", txt));
         }
     }
 
@@ -34,6 +44,11 @@ impl Logger {
         if self.loglevel >= WARN {
             println!("{}", self.format("warn", txt));
         }
+    }
+
+    pub fn error(&self, txt: &str) {
+        println!("{}", self.format("fatal error", txt));
+        exit(1);
     }
 }
 

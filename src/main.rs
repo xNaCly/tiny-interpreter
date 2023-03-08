b6231719c39602885b8ad2487a4250d73b078eaa
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-03-08 16:18:21 +0100

    refactor(repl): move repl to own module

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #![allow(dead_code)]
 use std::{
     env,
-    io::{self, Write},
     sync::{Arc, RwLock},
 };
 
+mod file;
 mod lexer;
 mod logger;
+mod repl;
 mod token;
 mod util;
 
@@ -14,6 +15,7 @@ use logger::{log, Logger};
 
 thread_local! {
     static LOG: RwLock<Arc<Logger>> = RwLock::new(Arc::new(Logger {
+        prefix: String::from("ti"),
         loglevel: logger::PRINT,
     }));
 }
@@ -22,40 +24,35 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let a: util::Arguments = util::parse_arguments(&args);
 
-    log().debug(&format!("process arguments: {:?}", args));
-
     LOG.with(|l| {
         *l.write().unwrap() = Arc::new(Logger {
             loglevel: a.loglevel,
+            prefix: "ti".to_string(),
         })
     });
 
+    log().debug(&format!("process arguments: {:?}", args));
+    log().debug(&format!("parsed arguments: {:?}", a));
+
     util::load_dot_env();
 
-    println!("\ntiny-interpreter - written by xnacly :^)");
-    println!("---");
-    println!("Type .exit to exit the interpreter");
-    let start_time = std::time::Instant::now();
-
-    loop {
-        print!("> ");
-        io::stdout().flush().unwrap();
-
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line from stdin");
-
-        match input.trim() {
-            ".exit" => break,
-            _ => (),
-        }
-
-        let mut lexer = lexer::Lexer::new(input.trim().to_string());
-        let lex_output = lexer.lex();
-        if lex_output.len() > 0 {
-            dbg!(lex_output);
-        }
+    if a.mode == util::RunMode::Repl {
+        repl::repl();
+    } else {
+        let input_file = match a.in_file_path {
+            Some(p) => p,
+            None => {
+                log().error("no input file specified");
+                return;
+            }
+        };
+        let output_file = match a.out_file_path {
+            Some(p) => p,
+            None => {
+                log().error("no output file specified");
+                return;
+            }
+        };
+        file::file(&input_file, &output_file);
     }
-    println!("exit, time in repl: {}s", start_time.elapsed().as_secs());
 }

87e8841f2f2d04e72653bb2a9dca157fadd12f9c
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-03-08 16:16:51 +0100

    feat(util): add help, rework args parsing

--- src/util.rs
+++ src/util.rs
@@ -1,15 +1,32 @@
+use core::panic;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::process::exit;
 
 use crate::logger::log;
 
-/// struct holding cli arguments
-pub struct Arguments {
-    pub loglevel: u8,
+#[derive(Debug, PartialEq)]
+pub enum RunMode {
+    Repl,
+    File,
 }
 
-const DEFAULT_ARGUMENTS: Arguments = Arguments { loglevel: 1 };
+/// struct holding cli arguments
+#[derive(Debug)]
+pub struct Arguments {
+    pub loglevel: u8,
+    pub mode: RunMode,
+    pub in_file_path: Option<String>,
+    pub out_file_path: Option<String>,
+}
+
+const DEFAULT_ARGUMENTS: Arguments = Arguments {
+    loglevel: 1,
+    mode: RunMode::Repl,
+    in_file_path: None,
+    out_file_path: None,
+};
 
 /// Loads the `.env` file located relatively from the current `cwd`.
 /// Does nothing if no `.env` file is present. This behavior is useful for switching from dev to prod env.
@@ -60,36 +77,96 @@ pub fn get_env(key: &str) -> String {
     }
 }
 
+/// parses the cli arguments and returns a struct containing the parsed arguments
 pub fn parse_arguments(args: &Vec<String>) -> Arguments {
     if args.len() == 1 {
         return DEFAULT_ARGUMENTS;
     }
 
-    let mut arguments = Arguments { loglevel: 1 };
-    let flag_arg = args.get(1).unwrap().as_str();
+    let mut arguments = Arguments {
+        loglevel: 1,
+        mode: RunMode::Repl,
+        out_file_path: None,
+        in_file_path: None,
+    };
 
-    if flag_arg.starts_with("-L") {
-        let formatted_arg = flag_arg.replace("-L", "");
-        match formatted_arg.as_str() {
-            "none" => arguments.loglevel = crate::logger::NONE,
-            "debug" => arguments.loglevel = crate::logger::DEBUG,
-            a => {
-                let a_as_int: u8 = match a.parse::<u8>() {
-                    Ok(a) => a,
-                    Err(b) => {
-                        log().warn(&format!("value '{}' not supported for -L: {}", a, b));
-                        return arguments;
+    let output_file = args.last().unwrap();
+
+    if output_file == "-o" {
+        log().error("no output file specified");
+    } else {
+        arguments.out_file_path = Some(output_file.to_string());
+    }
+
+    for (_, arg) in args.iter().skip(1).enumerate() {
+        if arg == "--help" || arg == "-h" {
+            help(args.get(0).unwrap(), false);
+        }
+        match arg.get(0..2) {
+            Some(value) => match value {
+                "-L" => match arg.get(2..).expect("no loglevel given") {
+                    "none" => arguments.loglevel = crate::logger::NONE,
+                    "debug" => arguments.loglevel = crate::logger::DEBUG,
+                    a => {
+                        let a_as_int: u8 = match a.parse::<u8>() {
+                            Ok(a) => a,
+                            Err(b) => {
+                                log().warn(&format!(
+                                    "value '{}' not supported for -L: {}, reseting to loglevel 1",
+                                    a, b
+                                ));
+                                return arguments;
+                            }
+                        };
+                        if !(crate::logger::NONE <= a_as_int && a_as_int <= crate::logger::DEBUG) {
+                            log().warn(&format!(
+                                "value '{}' not supported for -L, reseting to loglevel 1",
+                                a
+                            ));
+                        }
+                        arguments.loglevel = a_as_int
                     }
-                };
-                if !(crate::logger::NONE <= a_as_int && a_as_int <= crate::logger::DEBUG) {
-                    log().warn(&format!("value '{}' not supported for -L", a));
+                },
+                _ => {
+                    if arg.chars().nth(0).expect("no char at index 0") == '-' {
+                        log().warn(&format!("unrecognized command-line option '{}'", arg));
+                        help(args.get(0).unwrap(), true);
+                    } else {
+                        arguments.mode = RunMode::File;
+                        if arg != output_file {
+                            arguments.in_file_path = Some(arg.to_string());
+                        }
+                    }
                 }
-                arguments.loglevel = a_as_int
+            },
+            None => {
+                return arguments;
             }
         }
     }
 
     arguments
+}
+
+fn help(process_name: &str, short: bool) {
+    if short {
+        println!("usage: {} [options] file...", process_name);
+    } else {
+        let help_str = "usage: {} [options] file... \
+                        \nOptions:\
+                        \n\t-L<loglevel>    set the loglevel (0-3) (default: 1)\
+                        \n\t\t 0: none\
+                        \n\t\t 1: warn\
+                        \n\t\t 2: print\
+                        \n\t\t 3: debug
+                        \n\t-h, --help      display this help and exit\
+                        \n\t-o <file>       write output to <file>\
+                        \n\nThe output file is the last argument, if it is not a file, but a flag, the program will panic.\
+                        \nThis behaviour can be overriden by manually specifying an output file using the -o <file> flag.\
+                        ";
+        println!("{}", help_str)
+    }
+    exit(127)
 }
 
 #[cfg(test)]

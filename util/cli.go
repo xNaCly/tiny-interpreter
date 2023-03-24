d5e7689eccb1d11d80209bcfaa94ee3ae99bcb66
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-03-24 19:34:58 +0100

    feat(util): cli parsing

--- util/cli.go
+++ util/cli.go
@@ -1,1 +1,18 @@
-package cli
+// contains everything regarding the command line interface of teo
+package util
+
+import (
+	"os"
+)
+
+type TeoArguments struct {
+	InFile string
+}
+
+// defines and parses cli arguments
+func Arguments() TeoArguments {
+	args := os.Args
+	return TeoArguments{
+		InFile: args[1],
+	}
+}

0d032210ac539de1d471b36348ccd98eac6f2dc3
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-03-24 19:34:43 +0100

    feat(util): logger

--- teo.go
+++ teo.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/xnacly/tiny-interpreter/util"
+)
+
+// get cli arguments
+// check if input file, if true interpret, if false start repl
+// check if output file, if false print to stdout, if true write to file
+
+func main() {
+	args := util.Arguments()
+	util.L(args)
+	util.L("test")
+	util.LInfo("test")
+	util.LWarn("test")
+	util.LError("test")
+}
